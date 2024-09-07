949ed03d0a93adb0d0cfb89c72fefecff7bc3883
mklaric <marko.klaric@crosig.hr>
2024-09-07 13:19:47 +0200

    a

--- vars/buildTest.groovy
+++ vars/buildTest.groovy
@@ -1,3 +1,3 @@
 def call() {
-  println ('test')
+  println ('test123')
 }
--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,6 +1,8 @@
 def call(String repoUrl, String branch){
+   println("checking out code")
    def workingDir = "${env.WORKSPACE}"
    sh "git clone ${repoUrl} ${workingDir}"
    sh "git checkout ${branch}"
+   println("END CHECKING OUT")
    return workingDir
 }
